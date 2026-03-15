8618c2435064158450585c3df7f8a32a67bcb758
dongpengcheng <happydpc@sina.com>
2026-03-15 10:54:47 +0800

    feat: add typed ids for truck bop data structures

--- truck-bop/src/bopds/mod.rs
+++ truck-bop/src/bopds/mod.rs
@@ -1,33 +1,11 @@
 //! Boolean operation data structure
 
+mod ids;
+
+pub use ids::{
+    CommonBlockId, EdgeId, FaceId, PaveBlockId, SectionCurveId, ShapeId, VertexId,
+};
+
 /// Data structure
 #[derive(Debug)]
 pub struct BopDs;
-
-/// Shape identifier
-#[derive(Debug)]
-pub struct ShapeId;
-
-/// Vertex identifier
-#[derive(Debug)]
-pub struct VertexId;
-
-/// Edge identifier
-#[derive(Debug)]
-pub struct EdgeId;
-
-/// Face identifier
-#[derive(Debug)]
-pub struct FaceId;
-
-/// Pave block identifier
-#[derive(Debug)]
-pub struct PaveBlockId;
-
-/// Common block identifier
-#[derive(Debug)]
-pub struct CommonBlockId;
-
-/// Section curve identifier
-#[derive(Debug)]
-pub struct SectionCurveId;
