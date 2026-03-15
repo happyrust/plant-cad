85edd8c3298cc24c1d7c37c27a0bda3fd4a5ea76
dongpengcheng <happydpc@sina.com>
2026-03-15 10:54:05 +0800

    feat: add boolean pipeline placeholders

--- truck-bop/src/pipeline.rs
+++ truck-bop/src/pipeline.rs
@@ -1,5 +1,28 @@
 //! Boolean operation pipeline
 
 /// Boolean operation type
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum BooleanOp {
+    /// Union/Fuse
+    Fuse,
+    /// Intersection/Common
+    Common,
+    /// Subtraction/Cut
+    Cut,
+    /// Section
+    Section,
+}
+
+/// Pipeline execution report
 #[derive(Debug)]
-pub enum BooleanOp {}
+pub struct PipelineReport;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn boolean_op_debug_name_is_stable() {
+        assert_eq!(format!("{:?}", BooleanOp::Common), "Common");
+    }
+}
