e3b71720422c0f585ac6445aab2c7804e2cbe875
dongpengcheng <happydpc@sina.com>
2026-03-15 10:58:25 +0800

    feat: add public truck-bop boolean api stubs

--- truck-bop/src/lib.rs
+++ truck-bop/src/lib.rs
@@ -22,3 +22,50 @@ pub use bopds::{BopDs, CommonBlockId, EdgeId, FaceId, PaveBlockId, SectionCurveI
 pub use error::BopError;
 pub use options::BopOptions;
 pub use pipeline::BooleanOp;
+
+/// Common (intersection) operation stub
+pub fn common<C, S>(
+    _a: &truck_topology::Solid<truck_base::cgmath64::Point3, C, S>,
+    _b: &truck_topology::Solid<truck_base::cgmath64::Point3, C, S>,
+    _tol: f64,
+) -> Result<truck_topology::Solid<truck_base::cgmath64::Point3, C, S>, BopError> {
+    Err(BopError::NotImplemented("common"))
+}
+
+/// Fuse (union) operation stub
+pub fn fuse<C, S>(
+    _a: &truck_topology::Solid<truck_base::cgmath64::Point3, C, S>,
+    _b: &truck_topology::Solid<truck_base::cgmath64::Point3, C, S>,
+    _tol: f64,
+) -> Result<truck_topology::Solid<truck_base::cgmath64::Point3, C, S>, BopError> {
+    Err(BopError::NotImplemented("fuse"))
+}
+
+/// Cut (difference) operation stub
+pub fn cut<C, S>(
+    _a: &truck_topology::Solid<truck_base::cgmath64::Point3, C, S>,
+    _b: &truck_topology::Solid<truck_base::cgmath64::Point3, C, S>,
+    _tol: f64,
+) -> Result<truck_topology::Solid<truck_base::cgmath64::Point3, C, S>, BopError> {
+    Err(BopError::NotImplemented("cut"))
+}
+
+/// Section operation stub
+pub fn section<C, S>(
+    _a: &truck_topology::Solid<truck_base::cgmath64::Point3, C, S>,
+    _b: &truck_topology::Solid<truck_base::cgmath64::Point3, C, S>,
+    _tol: f64,
+) -> Result<truck_topology::Shell<truck_base::cgmath64::Point3, C, S>, BopError> {
+    Err(BopError::NotImplemented("section"))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn common_stub_reports_not_implemented() {
+        let err = BopError::NotImplemented("common");
+        assert!(matches!(err, BopError::NotImplemented(_)));
+    }
+}
